f0ac2be2436f989530f9ba99f60bc0f1f9acfad3
Raaking <raakin.24@ichat.sp.edu.sg>
2025-05-24 21:44:40 +0800

    fixed bmi calc

--- bmi.py
+++ bmi.py
@@ -1,7 +1,7 @@
 def calculate_bmi(height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
-    bmi = weight/(height**height)
+    bmi = weight/(height*height)
     print("bmi: " + str(bmi))
     if (bmi<18.5):
         print("underweight")
