5d12788a9e01cef996dce3af2a61f6a097372bd1
Raaking <raakin.24@ichat.sp.edu.sg>
2025-05-24 16:16:26 +0800

    Return changes

--- bmi.py
+++ bmi.py
@@ -5,12 +5,19 @@ def calculate_bmi(height, weight):
     print("bmi: " + str(bmi))
     if (bmi<18.5):
         print("underweight")
+        return -1
     elif(18.5<bmi<25.0):
         print("normalweight")
+        return 0
     else:
         print("overweight")
+        return 1
 
-calculate_bmi(weight=57, height=1.73)
+def main():
+    output = calculate_bmi(weight=57, height=1.73)
+    print("Return value based on weight classification: ", output)
 
+if __name__ == "__main__":
+    main()
 
 
